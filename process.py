1cee6ea839a473f3fda38f1d6333b7b5ff0a6d1a
JamaiKen <kgbailey92@gmail.com>
2016-12-07 14:43:31 -0600

    removed unnecessary wording

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 # Author: Kenneth Bailey
 # Date: 12/6/2016
-# Prog: python script to process new illegal downloads
+# Prog: python script to process new downloads
 
 import zipfile as zz
 import os 
